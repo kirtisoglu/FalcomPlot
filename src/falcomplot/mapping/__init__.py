d0381273ba5d932a460e0d7bed2f30a50ca480dd
kirtisoglu <77515254+kirtisoglu@users.noreply.github.com>
2026-03-08 16:22:18 -0500

    remove call_data.py

--- src/falcomplot/mapping/__init__.py
+++ src/falcomplot/mapping/__init__.py
@@ -12,19 +12,8 @@ Quickstart
     m = mapping.build_basemap("path/to/boundary.pkl")
     mapping.add_markers(m, facilities)
     m.save("interactive_map.html")
-
-call_data
----------
-An example data module for Chicago health facilities is included temporarily.
-It will move to a separate data library in a future release::
-
-    from falcomplot.mapping import call_data
-
-    boundary   = call_data.load_boundary()
-    facilities = call_data.fetch_all()
 """
 
 from .plott import build_basemap, add_hierarchy, add_markers, HEALTH_CATEGORIES
-from . import call_data
 
-__all__ = ["build_basemap", "add_hierarchy", "add_markers", "HEALTH_CATEGORIES", "call_data"]
+__all__ = ["build_basemap", "add_hierarchy", "add_markers", "HEALTH_CATEGORIES"]
